6331740b2e15b3ac08eb0b78dfe5675c36092277
keenset1 <laurent14511@gmail.com>
2023-02-12 15:55:38 -0500

    button func done

---
+++ src/ProductList.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useAuth } from './AuthContext';
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const { user, isAuthenticated } = useAuth();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await axios.get('http://localhost:5000/products');
+        setProducts(res.data);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  const handleAddToCart = async (productId) => {
+    if (!isAuthenticated) {
+      alert('Please log in to add products to your cart.');
+      return;
+    }
+
+    try {
+      await axios.post(`http://localhost:5000/cart`, {
+        userId: user.id,
+        productId,
+      });
+      alert('Product added to your cart.');
+    } catch (error) {
+      console.error(error);
+      alert('Error adding product to your cart.');
+    }
+  };
+
+  return (
+    <div className="product-list">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name}
+              <button onClick={() => handleAddToCart(product.id)}>
+                Add to cart
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
+
--- src/Products.js
+++ src/Products.js
@@ -1,21 +1,139 @@
-import React, { Component } from 'react'
-import MultiActionAreaCard from './MultiActionAreaCard'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-export default class Products extends Component {
-  render() {
-    return (
-        <center>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [errors, setErrors] = useState({});
 
-        
-      <div><b>Available Products:</b> 
-      
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await axios.get("/api/products");
+      setProducts(res.data);
+    };
+    fetchProducts();
+    useEffect(() => {
+      const fetchProducts = async () => {
+        const res = await axios.get("/api/products");
+        setProducts(res.data);
+      };
+      fetchProducts();
+    
+      const addExampleProducts = async () => {
+        const appleAccessories = [
+          {
+            name: "Apple AirPods Pro",
+            description: "Wireless earbuds with active noise cancellation",
+            price: "219.00",
+          },
+          {
+            name: "Apple Magic Keyboard",
+            description: "Wireless keyboard with numeric keypad",
+            price: "149.00",
+          },
+          {
+            name: "Apple Pencil",
+            description: "Stylus for iPad and iPhone",
+            price: "99.00",
+          },
+        ];
+    
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        };
+    
+        for (const accessory of appleAccessories) {
+          const body = JSON.stringify(accessory);
+          await axios.post("/api/products", body, config);
+        }
+      };
+    
+      addExampleProducts();
+    }, []);
+    
+  }, []);
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
-        <MultiActionAreaCard />
+    if (!name || !description || !price) {
+      setErrors({
+        name: !name ? "Name is required" : "",
+        description: !description ? "Description is required" : "",
+        price: !price ? "Price is required" : "",
+      });
+      return;
+    }
 
-      </div>
-      </center>
-    )
-  }
-}
+    try {
+      const newProduct = {
+        name,
+        description,
+        price,
+      };
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const body = JSON.stringify(newProduct);
+
+      const res = await axios.post("/api/products", body, config);
+
+      setProducts([...products, res.data]);
+      setName("");
+      setDescription("");
+      setPrice("");
+    } catch (err) {
+      setErrors({
+        ...errors,
+        server: err.response.data,
+      });
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        {errors.name && <p>{errors.name}</p>}
+        <input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        {errors.description && <p>{errors.description}</p>}
+        <input
+          type="text"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        {errors.price && <p>{errors.price}</p>}
+        <button type="submit">Add Product</button>
+      </form>
+      <ul>
+        {products.map((product) => (
+          <li key={product._id}>
+            <a href={`/products/${product._id}`}>{product.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ProductsList;
+
