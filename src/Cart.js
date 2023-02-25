74f377e80f0330a1d149e5d8100654964dfcde4f
Trinisia Fortune <trinisiafortune@trinisias-mbp.myfiosgateway.com>
2023-02-25 17:19:11 -0500

    lets get it

--- src/Cart.js
+++ src/Cart.js
@@ -1,73 +1,73 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useAuth } from './AuthContext';
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { useAuth } from './AuthContext';
 
-const Cart = () => {
-  const [cart, setCart] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [total, setTotal] = useState(0);
-  const { user } = useAuth();
+// const Cart = () => {
+//   const [cart, setCart] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [total, setTotal] = useState(0);
+//   const { user } = useAuth();
 
-  useEffect(() => {
-    const fetchCart = async () => {
-      try {
-        const res = await axios.get(`http://localhost:5000/cart/${user.id}`);
-        setCart(res.data);
-        setTotal(res.data.reduce((acc, curr) => acc + curr.price, 0));
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
-      }
-    };
-    fetchCart();
-  }, [user]);
+//   useEffect(() => {
+//     const fetchCart = async () => {
+//       try {
+//         const res = await axios.get(`http://localhost:5000/cart/${user.id}`);
+//         setCart(res.data);
+//         setTotal(res.data.reduce((acc, curr) => acc + curr.price, 0));
+//         setLoading(false);
+//       } catch (error) {
+//         console.error(error);
+//         setLoading(false);
+//       }
+//     };
+//     fetchCart();
+//   }, [user]);
 
-  const handleRemoveFromCart = async (productId) => {
-    try {
-      await axios.delete(`http://localhost:5000/cart/${user.id}/${productId}`);
-      setCart(cart.filter((p) => p.id !== productId));
-      setTotal(total - cart.find((p) => p.id === productId).price);
-    } catch (error) {
-      console.error(error);
-      alert('Error removing product from your cart.');
-    }
-  };
+//   const handleRemoveFromCart = async (productId) => {
+//     try {
+//       await axios.delete(`http://localhost:5000/cart/${user.id}/${productId}`);
+//       setCart(cart.filter((p) => p.id !== productId));
+//       setTotal(total - cart.find((p) => p.id === productId).price);
+//     } catch (error) {
+//       console.error(error);
+//       alert('Error removing product from your cart.');
+//     }
+//   };
 
-  const handleClearCart = async () => {
-    try {
-      await axios.delete(`http://localhost:5000/cart/${user.id}`);
-      setCart([]);
-      setTotal(0);
-    } catch (error) {
-      console.error(error);
-      alert('Error clearing your cart.');
-    }
-  };
+//   const handleClearCart = async () => {
+//     try {
+//       await axios.delete(`http://localhost:5000/cart/${user.id}`);
+//       setCart([]);
+//       setTotal(0);
+//     } catch (error) {
+//       console.error(error);
+//       alert('Error clearing your cart.');
+//     }
+//   };
 
-  return (
-    <div className="cart">
-      {loading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <h2>Your cart</h2>
-          <ul>
-            {cart.map((product) => (
-              <li key={product.id}>
-                {product.name} ({product.price}$)
-                <button onClick={() => handleRemoveFromCart(product.id)}>
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
-          <div>Total: {total}$</div>
-          <button onClick={handleClearCart}>Clear cart</button>
-        </>
-      )}
-    </div>
-  );
-};
+//   return (
+//     <div className="cart">
+//       {loading ? (
+//         <div>Loading...</div>
+//       ) : (
+//         <>
+//           <h2>Your cart</h2>
+//           <ul>
+//             {cart.map((product) => (
+//               <li key={product.id}>
+//                 {product.name} ({product.price}$)
+//                 <button onClick={() => handleRemoveFromCart(product.id)}>
+//                   Remove
+//                 </button>
+//               </li>
+//             ))}
+//           </ul>
+//           <div>Total: {total}$</div>
+//           <button onClick={handleClearCart}>Clear cart</button>
+//         </>
+//       )}
+//     </div>
+//   );
+// };
 
-export default Cart;
+// export default Cart;
--- src/Products.js
+++ src/Products.js
@@ -1,139 +1,113 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
-const ProductsList = () => {
-  const [products, setProducts] = useState([]);
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [price, setPrice] = useState("");
-  const [errors, setErrors] = useState({});
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await axios.get("/api/products");
-      setProducts(res.data);
-    };
-    fetchProducts();
-    useEffect(() => {
-      const fetchProducts = async () => {
-        const res = await axios.get("/api/products");
-        setProducts(res.data);
-      };
-      fetchProducts();
-    
-      const addExampleProducts = async () => {
-        const appleAccessories = [
-          {
-            name: "Apple AirPods Pro",
-            description: "Wireless earbuds with active noise cancellation",
-            price: "219.00",
-          },
-          {
-            name: "Apple Magic Keyboard",
-            description: "Wireless keyboard with numeric keypad",
-            price: "149.00",
-          },
-          {
-            name: "Apple Pencil",
-            description: "Stylus for iPad and iPhone",
-            price: "99.00",
-          },
-        ];
-    
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        };
-    
-        for (const accessory of appleAccessories) {
-          const body = JSON.stringify(accessory);
-          await axios.post("/api/products", body, config);
-        }
-      };
-    
-      addExampleProducts();
-    }, []);
-    
-  }, []);
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-
-    if (!name || !description || !price) {
-      setErrors({
-        name: !name ? "Name is required" : "",
-        description: !description ? "Description is required" : "",
-        price: !price ? "Price is required" : "",
-      });
-      return;
-    }
-
-    try {
-      const newProduct = {
-        name,
-        description,
-        price,
-      };
-
-      const config = {
-        headers: {
-          "Content-Type": "application/json",
-        },
-      };
-
-      const body = JSON.stringify(newProduct);
-
-      const res = await axios.post("/api/products", body, config);
-
-      setProducts([...products, res.data]);
-      setName("");
-      setDescription("");
-      setPrice("");
-    } catch (err) {
-      setErrors({
-        ...errors,
-        server: err.response.data,
-      });
-    }
-  };
-
+export default function MultiActionAreaCard() {
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+    <center>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require(`./images/watch.jpeg`)}
+          alt="Apple Watch"
         />
-        {errors.name && <p>{errors.name}</p>}
-        <input
-          type="text"
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            (F)Apple Watch
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          add to cart
+        </Button>
+      </CardActions>
+    </Card>
+
+    &nbsp;&nbsp;
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require(`./images/iphone.jpeg`)}
+          alt="Apple Watch"
         />
-        {errors.description && <p>{errors.description}</p>}
-        <input
-          type="text"
-          placeholder="Price"
-          value={price}
-          onChange={(e) => setPrice(e.target.value)}
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            (F)iPhone
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          add to cart
+        </Button>
+      </CardActions>
+    </Card>
+
+    &nbsp;&nbsp;
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require(`./images/ipad.jpeg`)}
+          alt="Apple Watch"
         />
-        {errors.price && <p>{errors.price}</p>}
-        <button type="submit">Add Product</button>
-      </form>
-      <ul>
-        {products.map((product) => (
-          <li key={product._id}>
-            <a href={`/products/${product._id}`}>{product.name}</a>
-          </li>
-        ))}
-      </ul>
-    </div>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            (F)iPad
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          add to cart
+        </Button>
+      </CardActions>
+    </Card>
+
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={require(`./images/airpods.jpeg`)}
+          alt="Apple Watch"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            (F)airpods
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          add to cart
+        </Button>
+      </CardActions>
+    </Card>
+    </center>
   );
-};
-
-export default ProductsList;
-
+}
